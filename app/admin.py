57216e00ae2bc749d968a319d739d99d0a162b91
xiidot1303 <xii1303@inbox.ru>
2024-07-30 15:15:46 +0500

    add location link on visit admin

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from app.models import *
 from rangefilter.filters import DateRangeFilter
 
 class VisitAdmin(admin.ModelAdmin):
     change_list_template = 'admin/visit/visit_change_list.html'
-    list_display = ['type', 'bot_user', 'address', 'comment', 'datetime', 'location']
+    list_display = ['type', 'bot_user', 'address', 'comment', 'datetime', 'location_link']
     search_fields = ['address']
     list_filter = ('bot_user', 'type', ('datetime', DateRangeFilter))
+
+    def location_link(self, obj):
+        link = f"https://www.google.com/maps?q={obj.lat},{obj.lon}"
+        return format_html(
+            '<a href="{}">{}</a>', 
+            link, 
+            obj.location if obj.location else link
+            )
+
 
 class DoctorAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
