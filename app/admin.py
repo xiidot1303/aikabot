fbdec835b69a4af3d96f9e9b2a884523c8ae7569
xiidot1303 <xii1303@inbox.ru>
2024-07-30 14:41:32 +0500

    add date range filter od Visit in admin

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from app.models import *
-
+from rangefilter.filters import DateRangeFilter
 
 class VisitAdmin(admin.ModelAdmin):
     change_list_template = 'admin/visit/visit_change_list.html'
     list_display = ['type', 'bot_user', 'address', 'comment', 'datetime', 'location']
     search_fields = ['address']
-    list_filter = ('bot_user', 'type',)
+    list_filter = ('bot_user', 'type', ('datetime', DateRangeFilter))
 
 class DoctorAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
